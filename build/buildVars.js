c081de9574c9eff70e153230f9a77ed7faf9d18c
huixisheng <469184346@qq.com>
2021-03-25 13:54:43 +0800

    fix: 修复项目启动运行报错
    fixed https://github.com/gzgogo/antd-theme/issues/17

--- build/buildVars.js
+++ build/buildVars.js
@@ -9,16 +9,31 @@ const rl = readline.createInterface({
   crlfDelay: Infinity
 });
 
+let multiLine = '';
 rl.on('line', (line) => {
-  // console.log(line);
   if (line.startsWith('@')) {
-    const [name, value] = line.split(':');
-    if (name && value) {
-      const temp = value.split('//')[0]; // 去掉行尾注释
-      varsObj[name] = temp.replace(';', '').trimLeft();
+    if (line.includes(';') && !multiLine) {
+      setVarsObjValue(line);
+      multiLine = '';
     }
+    multiLine += line;
+  } else if (multiLine) {
+    multiLine += line;
+  }
+
+  if (multiLine.includes(';')) {
+    setVarsObjValue(multiLine);
+    multiLine = '';
   }
 });
+
+function setVarsObjValue(line) {
+  const [name, value] = line.split(':');
+  if (name && value) {
+    const temp = value.split('//')[0]; // 去掉行尾注释
+    varsObj[name] = temp.replace(';', '').trimLeft();
+  }
+}
 
 rl.on('close', () => {
   // console.log(varsObj);
@@ -28,6 +43,12 @@ rl.on('close', () => {
       const value = varsObj[item.name];
       if (value) {
         item.value = item.type === 'number' ? parseFloat(value) : value;
+        // 兼容 type 定义异常的数据
+        if (Number.isNaN(item.value)) {
+          item.value = value;
+          item.type = 'string';
+          delete item.unit;
+        }
       }
     });
 
